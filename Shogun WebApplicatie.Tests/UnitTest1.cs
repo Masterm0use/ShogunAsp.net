50a8dba91c9d4a8746554453264d52947c93733d
Mario <mario.schipper@student.fontys.nl>
2016-01-15 14:38:38 +0100

    Terug waar het moet zijn
    Ik denk dat ik bij de vorige upload teveel heb gediscard.

--- Shogun WebApplicatie.Tests/UnitTest1.cs
+++ Shogun WebApplicatie.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shogun_WebApplicatie.Csharp;
 
 namespace Shogun_WebApplicatie.Tests
 {
@@ -7,8 +8,34 @@ namespace Shogun_WebApplicatie.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestClassKlant()
         {
+            DateTime testDatumBirth1 = new DateTime(2001, 1, 1);
+            DateTime testDatumBirth2 = new DateTime();
+
+            Klant k = new Klant(1, "123@test.nl", "TestWW", "Test", "Test", testDatumBirth1, "TestDatum", "BTW123456", "0613556430", true, true);
+            Klant k1 = new Klant(1, "123@test.nl", "TestWW", "Test", "Test", testDatumBirth2, "TestDatum", "BTW123456", "0613556430", true, true);
+
+            //Klant Data testen
+            Assert.AreEqual(k.Nieuwsbrief, true);
+            Assert.AreEqual(k.Abonnement, true);
+            Assert.AreEqual(k.Email, "123@test.nl");
+
+            Assert.AreEqual(k.ToString(), "123@test.nl - Test - Test - 0613556430 - 1-1-2001 00:00:00");
+            Assert.AreEqual(k1.ToString(), "123@test.nl - Test - Test - 0613556430 - 1-1-0001 00:00:00");
+        }
+
+        [TestMethod]
+        public void TestClassCategorie()
+        {
+            Categorie c = new Categorie(1, "Airsoft", 1);
+
+
+            Assert.AreEqual(c.CategorieNaam, "Airsoft");
+            Assert.AreEqual(c.ID, 1);
+            Assert.AreEqual(c.Parentid, 1);
+
+            Assert.AreEqual(c.ToString(), "Airsoft");
         }
     }
 }
--- Shogun WebApplicatie/Csharp/Klant.cs
+++ Shogun WebApplicatie/Csharp/Klant.cs
@@ -10,15 +10,20 @@ namespace Shogun_WebApplicatie.Csharp
         private bool abonnement;
         private bool nieuwsbrief;
 
-        public bool Nieuwsbrief { get; set; }
-        public bool Abonnement { get; set; }
+        public bool Nieuwsbrief { get { return nieuwsbrief; } }
+        public bool Abonnement { get { return abonnement; } }
 
 
-        public Klant(int id, string email, string wachtwoord, string voornaam, string achternaam, DateTime geboortedatum, string bedrijfsnaam, string btwNummer, string telefoonNummer, bool nieuwsbrief, bool abonnement) 
+        public Klant(int id, string email, string wachtwoord, string voornaam, string achternaam, DateTime geboortedatum, string bedrijfsnaam, string btwNummer, string telefoonNummer, bool nieuwsbrief, bool abonnement)
             : base(id, email, wachtwoord, voornaam, achternaam, geboortedatum, bedrijfsnaam, btwNummer, telefoonNummer)
         {
             this.abonnement = abonnement;
             this.nieuwsbrief = nieuwsbrief;
+        }
+
+        public override string ToString()
+        {
+            return Email + " - " + Voornaam + " - " + Achternaam + " - " + TelefoonNummer + " - " + Geboortedatum.ToString();
         }
     }
 }
